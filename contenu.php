7fc0d31079ce6fe0a91bb2c8798a079b65ea0b6c
elhaddadi-meroine96 <elhaddadi.meroine@gmail.com>
2018-12-13 11:00:17 +0100

    commit

--- contenu.php
+++ contenu.php
@@ -1,17 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="contenu.css" />
+
 </head>
 <body>
+
 <?php
-$_POST = array ('drone','message');
+if(isset($_POST['ajouter'])){
 
-?>
-</body>
-</html>
+// SANITIZING//
+
+    if(isset($_POST['message']) && $_POST['message'] != ""){
+        
+      $Sanitize = filter_input($_POST,'message', FILTER_SANITIZE_STRING);
+      $Validate = filter_input($_POST,'message', FILTER_VALIDATE_STRING);
+    }
+    }
+
+      function submit()
+    {
+     echo "<input type='checkbox'>";
+    }
 
 
+// JSON //
+    $contenu_json = json_encode('message');
+    $todo = 'todo.json';
+    $fichier = fopen($todo, 'w+');
+    fwrite($fichier, $contenu_json);
+    fclose($fichier);
+
+
+
+
+    ?>
+    </body>
+    </html>
--- formulaire.php
+++ formulaire.php
@@ -13,33 +13,34 @@
 
 <hr>
 <p> A FAIRE</p>
-<div>
-  <input type="checkbox" id="course" name="drone" value="faireSesCourses"
-         checked>
-  <label for="faireSesCourses">Faire les courses</label>
-</div>
-<div>
-  <input type="checkbox" id="qcm" name="drone" value="TerminerLeProjetQcm"
-        >
-  <label for="TerminerLeProjetQcm">Terminer le projet "QCM"</label>
-</div>
-<div>
-  <input type="checkbox" id="grandmere" name="drone" value="RappelerGrandMere"
-        >
-  <label for="RappelerGrandMere">Rappeler Grand-mère</label>
-</div>
+  <div>
+      <input type="checkbox" id="course" name="drone" value="faireSesCourses" checked>
+      <label for="faireSesCourses">Faire les courses</label>
+  </div>
+   <div>
+      <input type="checkbox" id="qcm" name="drone" value="TerminerLeProjetQcm">
+      <label for="TerminerLeProjetQcm">Terminer le projet "QCM"</label>
+  </div>
+   <div>  
+      <input type="checkbox" id="grandmere" name="drone" value="RappelerGrandMere">
+      <label for="RappelerGrandMere">Rappeler Grand-mère</label>
+   </div>
 <br>
-<input type="submit"  name="button" value="Enregistrer">
+<input type="submit"  name="enregistrer" value="Enregistrer" >
+
+<p>ARCHIVE<p>
+<div>
+
+</div>
+<!-- AJOUT DE TACHE ETAPE 1 -->
 
 <hr>
-<p> Ajouter une tâche</p>
+<h2> Ajouter une tâche</h2>
 <p> La tâche à effectuer</p>
 <textarea cols="50" id="message" name="message"></textarea>
 <br>
 <br>
-<input type="submit" name="button" value="Ajouter">
-
-</form>
-
+<input type="submit" name="ajouter" value="Ajouter" onclick="submit()">
+ </form>
 </body>
 </html>
